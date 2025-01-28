0e6cce670b22cb0ad9a09a8305187e66191d9aa9
Syllac21 <syllac.online@gmail.com>
2025-01-28 14:43:30 +0100

    recupération de la liste des promos en cours

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,33 @@
+// récupération des catégories
+async function fetchCat() {
+    const reponse = await fetch('http://portfolios.ruki5964.odns.fr/wp-json/wp/v2/promotions');
+    const listPromo = await reponse.json();
+    
+    return listPromo;
+    
+}
+// selecteur de catégorie
+fetchCat().then((listPromo) => {
+    const options = listPromo.map(promo => options.slug);
+
+    const textSelector = `<select name="categories" id="categories">
+        <option value="">---</option>
+        ${options}</select>`;
+    select.innerHTML = textSelector;
+    
+})
+
+// recupérer la liste des stagiaires
 async function recupList() {
     const reponse = await fetch('http://portfolios.ruki5964.odns.fr/wp-json/wp/v2/apprenants');
     const ListStagiaires = await reponse.json();
     console.log(ListStagiaires);
 }
-recupList();
+recupList().then((ListStagiaires)=>{
+    console.log(ListStagiaires)
+    let table = [];
+
+})
+    // ListStagiaires.forEach(stagiaire => {
+    //             let apprenant = {"nom"=> stagiaire.nom}
+    // });
