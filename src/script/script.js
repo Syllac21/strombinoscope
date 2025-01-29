34f1054fa1094635094d29a34f903a64dd55394f
Syllac21 <syllac.online@gmail.com>
2025-01-29 15:19:28 +0100

    filtre par promo et par compétences ok

--- src/script/script.js
+++ src/script/script.js
@@ -1,7 +1,8 @@
 const select = document.getElementById('select');
 let filterComp = [];
+let filterPromo = '';
 
-// récupération des catégories
+// récupération des promotions
 async function fetchPromo() {
     const reponse = await fetch('http://portfolios.ruki5964.odns.fr/wp-json/wp/v2/promotions');
     const listPromo = await reponse.json();
@@ -9,40 +10,52 @@ async function fetchPromo() {
     
 }
 
-// selecteur de catégorie
+// selecteur de promotion
 fetchPromo().then((listPromo) => {
-    // console.log(listPromo[0]);
-    const options = listPromo.map(promo => promo.slug);
-    const optionsText = options.map(promo =>`<option value = "${promo}">${promo}</option>`).join('');
+    // const options = listPromo.map(promo => promo.slug);
+    console.log(listPromo);
+    const optionsText = listPromo.map(promo =>`<option value = "${promo.id}">${promo.slug}</option>`).join('');
 
-    const textSelector = `<select name="categories" id="categories">
+    const textSelector = `<select name="promotions" id="promotions">
         <option value="">---</option>
         ${optionsText}</select>`;
     select.innerHTML = textSelector;
+    //écouter la liste déroulante et filtrer
+    const selectPromo = document.getElementById('promotions');
+    selectPromo.addEventListener('change', ()=>{
+       
+            filterPromo = selectPromo.value;
+            console.log(filterPromo);
+            filterStudent();
+        
+    })
 })
 
 // recupérer la liste des stagiaires
 async function recupList() {
     const reponse = await fetch('http://portfolios.ruki5964.odns.fr/wp-json/wp/v2/apprenants/?per_page=100');
     const listStagiaires = await reponse.json();
-    console.log(listStagiaires);
+    // console.log(listStagiaires);
     return listStagiaires;
 }
+
+filterCompetences();
 recupList().then((listStagiaires)=>{
     const grid = document.getElementById('grid');
-    filterCompetences();
     fetchCompetences().then((competences)=>{
         listStagiaires.forEach(stagiaire => {
+            fetchPromo().then((listPromo)=>{
+                grid.innerHTML+=createCard(stagiaire, competences, listPromo);
+            })
             // console.log(stagiaire.nom);
-            grid.innerHTML+=createCard(stagiaire, competences);
         });
     })
     
 })
 
-function createCard(stagiaire, competences){
+function createCard(stagiaire, competences, listPromo){
         let textCard =`<div class="card">
-                    <h4>${stagiaire.prenom} ${stagiaire.nom}  2024</h4>
+                    <h4>${stagiaire.prenom} ${stagiaire.nom}  ${textPromo(listPromo,stagiaire.promotions)}</h4>
                     <img src="${stagiaire.image}" alt="${stagiaire.nom}">
                     <p>${stagiaire.excerpt.rendered}</p>
                     <p>compétences : ${listeComp(competences,stagiaire.competences)}</p>
@@ -51,6 +64,16 @@ function createCard(stagiaire, competences){
                     
                 </div>`;
         return textCard;
+}
+function textPromo(listPromo,promo){
+    let slugPromo = '';
+    // console.log(listPromo);
+    listPromo.forEach((promotion)=>{
+        if(promotion.id === promo[0]){
+            slugPromo = promotion.slug;
+        }
+    })
+    return slugPromo;
 }
 
 function listeComp(competences, comps){
@@ -87,20 +110,57 @@ async function filterCompetences() {
         <input type="checkbox" id="competence-${competence.name}" name="${competence.name}" value="${competence.id}" class="check">
         `)
     .join("<br>");
+
+    //écouter les checkbox et filtrer
     const checkbox = document.querySelectorAll('.check');
-    console.log(checkbox);
     checkbox.forEach(check =>{
-        check.addEventListener('change',(e)=>{
+        check.addEventListener('change',()=>{
             if(check.checked){
                 filterComp.push(check.value);
-                console.log(filterComp);
-                filter();
+                filterStudent();
+            }else{
+                filterComp = filterComp.filter(function(f){return f !== check.value});
+                filterStudent();
             }
         })
     })
 }
 
-//filtre de la liste
-function filter(){
-    if(filterComp )
+
+
+// filtre de la liste
+async function filterStudent() {
+    try {
+        const listStagiaires = await recupList();
+        let list = listStagiaires;
+
+        // Filtrer par compétences
+        if (filterComp.length > 0) {
+            list = list.filter(stagiaire =>
+                filterComp.every(comp => stagiaire.competences.includes(Number(comp)))
+            );
+        }
+
+        // Filtrer par promotion
+        if (filterPromo !== '') {
+            list.forEach((stagiaire)=>{
+                console.log(Number(filterPromo),stagiaire.promotions[0])
+                list = list.filter(stagiaire => stagiaire.promotions[0] === Number(filterPromo));
+                
+            })
+        }
+
+        // Affichage de la liste filtrée
+        const grid = document.getElementById('grid');
+        grid.innerHTML = '';
+        const competences = await fetchCompetences();
+        const listPromo = await fetchPromo();
+
+        list.forEach(stagiaire => {
+            grid.innerHTML += createCard(stagiaire, competences, listPromo);
+        });
+    } catch (error) {
+        console.error('Erreur lors du filtrage des stagiaires:', error);
+    }
 }
+    
